54282d2b816da25ba51e00e3ebbe7114528fd766
Rahim337 <a_mahi@enst.dz>
2025-11-01 16:02:41 +0100

    final

---
+++ tp2/action.js
@@ -0,0 +1,23 @@
+window.onload = () => { 
+    
+    const paramsString = document.location.search;
+    const searchParams = new URLSearchParams(paramsString);
+  
+
+    for (const param of searchParams) {
+      console.log(param);
+  
+      const elementId = param[0];
+      const elementValue = param[1];
+      const element = document.getElementById(elementId);
+      
+      if (element !== null) {
+        element.textContent = elementValue;
+      }
+      if (param[0] === "address") {
+         element.href = `https://www.google.com/maps/search/?api=1&query=${elementValue}`;
+      } else if (param[0] === "email") {
+         element.href = `mailto:${elementValue}?subject=Hello!&body=What's up?`;
+      }
+    }
+  };
